8e78594bfbba53cd8d9468fd943e09b11c2764d0
jan@huggenberg.ch <jan@huggenberg.ch>
2021-06-16 15:29:06 +0200

    added heatmap.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,36 @@
 import { data } from "browserslist";
 import L from "leaflet";
+import HeatmapLayer from "leaflet-heatmap";
 import "leaflet/dist/leaflet.css";
-import satData from './data/satiliteData.json';
+import satData from "./data/ret100.json";
 
 function initApp() {
-  console.log(satData);
 
-  // init map
-  const map = L.map("map", {
-    center: [52.52, 13.4],
-    zoom: 4,
-  });
-
-  // configure tile layer
-  new L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png", {
+  // configure base tile layer
+  let baseLayer = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png", {
     attribution: `attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>`,
     detectRetina: true,
-  }).addTo(map);
+  });
+
+  var cfg = {
+    maxOpacity: 0.8,
+    blur: 1,
+    scaleRadius: false,
+    useLocalExtrema: true,
+    latField: "lat",
+    lngField: "lon",
+    valueField: "ret100",
+  };
+
+  let heatmap = new HeatmapLayer(cfg);
+
+  var map = new L.Map("map", {
+    center: new L.LatLng(-49.975, -74.825),
+    zoom: 6,
+    layers: [baseLayer, heatmap],
+  });
+
+  heatmap.setData(satData);
 
   // register events
   map.addEventListener("click", handleMapClick);
