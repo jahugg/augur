bcf5d26dcd53d6987762e30189556c3eacf9a034
jan@huggenberg.ch <jan@huggenberg.ch>
2021-06-16 11:31:34 +0200

    load data file

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import { data } from "browserslist";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import satData from './data/satiliteData.json';
 
 function initApp() {
+  console.log(satData);
 
   // init map
-  const map = L.map('map', {
+  const map = L.map("map", {
     center: [52.52, 13.4],
     zoom: 4,
   });
 
   // configure tile layer
-  new L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', {
+  new L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png", {
     attribution: `attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>`,
     detectRetina: true,
   }).addTo(map);
 
   // register events
-  map.addEventListener('click', handleMapClick);
+  map.addEventListener("click", handleMapClick);
 }
 
 // click event
