8c98831010e27e90fbf465bd4e339d6ef17286ce
jan@huggenberg.ch <jan@huggenberg.ch>
2021-06-30 11:54:36 +0200

    imported leaflet-draw

--- src/main.js
+++ src/main.js
@@ -1,36 +1,35 @@
-import { data } from "browserslist";
-import L from "leaflet";
-import HeatmapLayer from "leaflet-heatmap";
+import L from 'leaflet';
+import Draw from 'leaflet-draw';
 import "leaflet/dist/leaflet.css";
-import satData from "./data/ret100.json";
+import "leaflet-draw/dist/leaflet.draw.css"
 
 function initApp() {
-  // configure base tile layer
+  // configure tile layer
   let baseLayer = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png", {
     attribution: `attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>`,
     detectRetina: true,
   });
 
-  let heatmap = new HeatmapLayer({
-    maxOpacity: 0.8,
-    radius: 2,
-    scaleRadius: true,
-    useLocalExtrema: true,
-    latField: "lat",
-    lngField: "lon",
-    valueField: "ret100",
+  // create map
+  let map = new L.Map("map", {
+    center: new L.LatLng(0, 0),
+    zoom: 3,
+    drawControl: true,
+    layers: [baseLayer],
   });
 
-  var map = new L.Map("map", {
-    center: new L.LatLng(-49.975, -74.825),
-    zoom: 6,
-    layers: [baseLayer, heatmap],
+  // FeatureGroup is to store editable layers
+  let drawnItems = new L.FeatureGroup();
+  map.addLayer(drawnItems);
+  let drawControl = new L.Control.Draw({
+    edit: {
+      featureGroup: drawnItems,
+    },
   });
-
-  heatmap.setData(satData);
+  map.addControl(drawControl);
 
   // register events
-  map.addEventListener("click", handleMapClick);
+  // map.addEventListener("click", handleMapClick);
 }
 
 // click event
