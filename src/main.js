71fddae23cd7dc6f22b5f36cc3c06590f27b654c
jan@huggenberg.ch <jan@huggenberg.ch>
2021-06-30 16:33:03 +0200

    added custom marker

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-import L from 'leaflet';
-import Draw from 'leaflet-draw';
+import L from "leaflet";
+import Draw from "leaflet-draw";
 import "leaflet/dist/leaflet.css";
-import "leaflet-draw/dist/leaflet.draw.css"
+import "leaflet-draw/dist/leaflet.draw.css";
+import leaf from "url:./leaf-orange.png";
+import leafShadow from "url:./leaf-shadow.png";
+
+const locationMarker = L.icon({
+  iconUrl: leaf,
+  shadowUrl: leafShadow,
+
+  iconSize: [38, 95], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
 
 function initApp() {
   // configure tile layer
@@ -14,26 +27,40 @@ function initApp() {
   let map = new L.Map("map", {
     center: new L.LatLng(0, 0),
     zoom: 3,
-    drawControl: true,
     layers: [baseLayer],
   });
 
   // FeatureGroup is to store editable layers
   let drawnItems = new L.FeatureGroup();
   map.addLayer(drawnItems);
+
   let drawControl = new L.Control.Draw({
-    edit: {
-      featureGroup: drawnItems,
+    draw: {
+      marker: { icon: locationMarker },
+      polyline: false,
+      rectangle: false,
+      circle: false,
+      circlemarker: false,
     },
+    edit: false,
   });
   map.addControl(drawControl);
 
+  // event handling
+  map.addEventListener("draw:created", handleNewPolygone);
+
+  function handleNewPolygone(event) {
+    let layer = event.layer;
+    console.log(layer);
+    map.addLayer(layer);
+  }
+
   // register events
-  // map.addEventListener("click", handleMapClick);
+  // map.addEventListener("click", sendCoords);
 }
 
 // click event
-function handleMapClick(event) {
+function sendCoords(event) {
   alert(`COORDINATES: ${event.latlng}`);
   // send, calculate and fetch data about floods here
 }
